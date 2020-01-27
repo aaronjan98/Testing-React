d831f5462348181c1916e5e029034faad7dd5aae
aaronjan <aaronjan98@gmail.com>
2020-01-27 12:59:21 -0800

    added a test to check for the rendering of the button
    inside the StarWarsCharacters component using query Selectors, getByText

---
+++ star-wars-app/src/components/StarWarsCharacters.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import StarWarsCharacters from './StarWarsCharacters';
+
+test('renders StarWArsCharacters with the characters & previous/next buttons', () => {
+    // AAA Arrange, Act, Assert
+    
+})
