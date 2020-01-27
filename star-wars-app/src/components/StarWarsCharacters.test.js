25b3fa496fb253026becd03e55cbf87b0ad8cca5
aaronjan <aaronjan98@gmail.com>
2020-01-27 13:16:22 -0800

    imported fireEvent for the two buttons

--- star-wars-app/src/components/StarWarsCharacters.test.js
+++ star-wars-app/src/components/StarWarsCharacters.test.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import StarWarsCharacters from './StarWarsCharacters';
 
-test('renders StarWArsCharacters with the characters & previous/next buttons', () => {
+test('Renders StarWarsCharacters\' previous/next buttons', () => {
     // AAA Arrange, Act, Assert
-    
+    const { getByText } = render(<StarWarsCharacters />);
+    const previousButton = getByText(/Previous/i);
+    const nextButton = getByText(/Next/i);
+
+    fireEvent.change(previousButton);
+    fireEvent.change(nextButton);
 })
+
+// test('Previous & Next buttons fire the new call of characters from the API', () => {
+//     render(<StarWarsCharacters />);
+//     fireEvent.change()
+// })
